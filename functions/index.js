618dda6933845b58f7cc20bb8e3d7794fec01512
Walter Kuppens <reshurum@gmail.com>
2017-07-07 15:10:11 -0700

    Rewrote as a firebase function

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+const functions = require('firebase-functions');
+const IncomingWebhook = require('@slack/client').IncomingWebhook;
+
+// Slack webhook client used to post messages to a channel.
+const webhook = new IncomingWebhook(functions.config().slack.webhook);
+
+// A list of quotes that are randomly chosen from when an inquiry comes through.
+const quotes = [
+  "It appears someone wants to do business with us.",
+  "The word is getting out, we got a potential client.",
+  "Someone with good tastes wants to do some business.",
+  "A new proposition has come in.",
+  "Some new business came in.",
+  "A new client, hopefully not another peasant.",
+];
+
+// Posts a comment to a slack channel with a random quote and contact
+// information when a request is submitted to the firebase database.
+exports.postInquiry = functions.database.ref('/requests/{requestId}').onWrite((event) => {
+  const data = event.data.val();
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  const message = `${quote}
+
+Email: ${data.email}
+Name: ${data.name}
+Phone: ${data.phone}
+Referer: ${data.referer}
+Request: ${data.request}`;
+
+  return new Promise((resolve, reject) => {
+    webhook.send(message, (err, header, statusCode, body) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+});
